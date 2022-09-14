c5cdefe39a58bda5de798ea67da3f6a9597f5f7c
Ryan Cartner <ryan.cartner@clio.com>
2022-09-14 15:32:34 -0300

    flow analysis XSS test

--- app/controllers/echo_controller.rb
+++ app/controllers/echo_controller.rb
@@ -1,6 +1,9 @@
 class EchoController < ApplicationController
 
   def echo
+    @temp = params['name']
+    @upcased = @temp.upcase
+    @name = upcased + " - Transforming this variable"
   end
 
 end
